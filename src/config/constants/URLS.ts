c69d02c671d0420c569c5bfc15d17a2226979a0f
Jose <jjoseguilhermeneto@gmail.com>
2024-09-03 17:42:36 -0300

    Update URLS.ts (#3)

--- src/config/constants/URLS.ts
+++ src/config/constants/URLS.ts
@@ -6,22 +6,28 @@ const DOMAIN = ENVIRONMENT.DOMAIN;
 
 // SERVER
 // https://serverdomain.com OU http://localhost
-const SERVER_URL = `${PROTOCOL}${ENVIRONMENT.SERVER_SUBDOMAIN_DOMAIN}`;
+const SERVER_URL_WITH_PROTOCOL = `${PROTOCOL}${ENVIRONMENT.SERVER_URL}`;
 // SERVER PORT
 const SERVER_PORT = ENVIRONMENT.SERVER_PORT;
 // https://serverdomain.com OU http://localhost:PORT
-const SERVER_FULL_URL = ENVIRONMENT.MODE === 'DEVELOPMENT' ? `${SERVER_URL}:${SERVER_PORT}` : SERVER_URL;
+const SERVER_FULL_URL =
+  ENVIRONMENT.MODE.toUpperCase() === 'DEVELOPMENT'
+    ? `${SERVER_URL_WITH_PROTOCOL}:${SERVER_PORT}`
+    : SERVER_URL_WITH_PROTOCOL;
 // https://serverdomain.com/api OU http://localhost:PORT/api
-export const API_BASE = '/api';
+export const API_BASE = '';
 export const API_URL = `${SERVER_FULL_URL}${API_BASE}`;
 
 // CLIENT
 // https://clientdomain.com OU http://localhost
-const CLIENT_URL = `${PROTOCOL}${ENVIRONMENT.CLIENT_SUBDOMAIN_DOMAIN}`;
+const CLIENT_URL_WITH_PROTOCOL = `${PROTOCOL}${ENVIRONMENT.CLIENT_URL}`;
 /* CLIENT PORT */
 const CLIENT_PORT = ENVIRONMENT.CLIENT_PORT;
 /* https://client.domain.com OU http://localhost:PORT */
-export const CLIENT_FULL_URL = ENVIRONMENT.MODE === 'PRODUCTION' ? CLIENT_URL : `${CLIENT_URL}:${CLIENT_PORT}`;
+export const CLIENT_FULL_URL =
+  ENVIRONMENT.MODE.toUpperCase() === 'PRODUCTION'
+    ? CLIENT_URL_WITH_PROTOCOL
+    : `${CLIENT_URL_WITH_PROTOCOL}:${CLIENT_PORT}`;
 
 const ENDPOINTS = {
   // Example
@@ -41,9 +47,9 @@ const API_ENDPOINTS = {
 const URLS = {
   MAIN_URLS: {
     CLIENT_PORT,
-    CLIENT_URL,
+    CLIENT_URL_WITH_PROTOCOL,
     SERVER_PORT,
-    SERVER_URL,
+    SERVER_URL_WITH_PROTOCOL,
     API_URL,
     CLIENT_FULL_URL,
     SERVER_FULL_URL,
